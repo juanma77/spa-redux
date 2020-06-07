68a81197ee0822d8e3112fc708e5cf6fe08bba68
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-06-07 16:35:06 -0500

    store devtools dependency added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { NietoComponent } from './components/contador/nieto/nieto.component';
 // Ngrx
 import { StoreModule } from '@ngrx/store';
 import { contadorReducer } from './components/contador/contador.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
 
 
 @NgModule({
@@ -19,7 +21,15 @@ import { contadorReducer } from './components/contador/contador.reducer';
   imports: [
     BrowserModule,
     // El nombre del store es "contador"
-    StoreModule.forRoot({ contador: contadorReducer })
+    StoreModule.forRoot({ contador: contadorReducer }),
+
+    // Para poder usar la extensión de Devtools en Chrome
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Con esto decimos que el estado mantenga los útlimos 25 estados
+
+      // Para que los usuarios no manipulen el estado en producción 
+      logOnly: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
