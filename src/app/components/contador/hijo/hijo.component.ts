49742d9bbc681ee40e8985d4b102f609a4571999
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-06-07 10:27:54 -0500

    using Input and Output to pass properties between child and parent component

--- src/app/components/contador/hijo/hijo.component.ts
+++ src/app/components/contador/hijo/hijo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-hijo',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HijoComponent implements OnInit {
 
+  // Recibimos el contador desde el componente padre app.component
+  @Input() contador: number;
+
+
+  // Mandamos desde el componente hijo hijo.component la propiedad cambioContador con un Event Emitter hacia el componente padre app.component
+  @Output() cambioContador = new EventEmitter<number>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public multiplicar(){
+    this.contador *=2;
+
+    // Siempre debemos emitir el evento con la propiedad que estamos mandando como Output
+    this.cambioContador.emit(this.contador); 
+  }
+
+  public dividir(){
+    this.contador /=2;
+
+    this.cambioContador.emit(this.contador); 
+
   }
 
 }
